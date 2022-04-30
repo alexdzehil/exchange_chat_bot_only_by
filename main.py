a6aee3c8b2e52e0bc750eff9576ea25cb9a161ea
Alexandr Dzehil <a.dzhehil@4taps.me.com>
2022-04-30 22:24:38 +0300

    Add schedule

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/home/alex/Документы/exchange/env/bin/python
 import datetime
+import time
 
 from emoji import emojize
 import requests
+import schedule
 import telebot
 
 import _config
@@ -11,8 +13,8 @@ import _config
 bot = telebot.TeleBot(_config.TELEGRAM_KEY)
 
 
-def get_course(url):
-    request = requests.get(url)
+def get_course():
+    request = requests.get('https://belarusbank.by/api/kursExchange?city=Слоним')
     data = request.json()
 
     usd_in = data[0]['USD_in']
@@ -21,17 +23,21 @@ def get_course(url):
     eur_out = data[0]['EUR_out']
     rub_in = data[0]['RUB_in']
     rub_out = data[0]['RUB_out']
+    pln_in = data[0]['RUB_in']
+    pln_out = data[0]['RUB_out']
 
-    return usd_in, usd_out, eur_in, eur_out, rub_in, rub_out
+    return usd_in, usd_out, eur_in, eur_out, rub_in, rub_out, pln_in, pln_out
 
 
 def send_to_group(args):
-    usd_in, usd_out, eur_in, eur_out, rub_in, rub_out = args
+    usd_in, usd_out, eur_in, eur_out, rub_in, rub_out, pln_in, pln_out = args
     bank = emojize(_config.USER_EMOJI[0], language='alias')
     date = emojize(_config.USER_EMOJI[4], language='alias')
     eu_emoji = emojize(_config.USER_EMOJI[2], language='alias')
     ru_emoji = emojize(_config.USER_EMOJI[3], language='alias')
     dollar = emojize(_config.USER_EMOJI[1], language='alias')
+    polsca = emojize(_config.USER_EMOJI[5], language='alias')
+    polsca1 = emojize(_config.USER_EMOJI[6], language='alias')
     euro = '\u20ac'
     rub = '\u20BD'
     now = datetime.datetime.date(datetime.datetime.today())
@@ -44,7 +50,12 @@ def send_to_group(args):
         f"{dollar}    1$ - {usd_in}, {usd_out}\n"
         f"{eu_emoji}    1{euro} - {eur_in}, {eur_out}\n"
         f"{ru_emoji}100{rub} - {rub_in}, {rub_out}\n"
+        f"{polsca}{polsca1} 1Z - {pln_in}, {pln_out}"
     )
 
 
-send_to_group(get_course(_config.URL))
+schedule.every().day.at('11:00').do(send_to_group, get_course())
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
